cf8c538d2bcdf4cb452f5c29c2f12dfa4ed395c0
davidkvara <davekvaratskhelia@gmail.com>
2018-02-07 22:39:11 +0400

    added redux, created first store, combined reducers, dispatched actions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import React, { Component } from "react";
-// import Calculator from "./examples/calculator";
-// import Todo from "./examples/todo";
+import React from "react";
+import { combineReducers, createStore } from "redux";
 
-class App extends Component {
-  render() {
-    return <div className="App">Start Here</div>;
+const counter = (state = { count: 0 }, action) => {
+  if (action.type === "INCREMENT") {
+    return { ...state, count: state.count + action.amount };
   }
-}
+  if (action.type === "DECREMENT") {
+    return { ...state, count: state.count - action.amount };
+  }
+  return state;
+};
+
+const initialState = {
+  name: "user",
+  age: null
+};
+
+const user = (state = initialState, action) => {
+  if (action.type === "CHANGE_NAME") {
+    return { ...state, name: action.new_name };
+  }
+  if (action.type === "CHANGE_AGE") {
+    return { ...state, age: action.new_age };
+  }
+  return state;
+};
+
+const reducer = combineReducers({
+  counter: counter,
+  user: user
+});
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "INCREMENT", amount: 5 });
+store.dispatch({ type: "INCREMENT", amount: 15 });
+store.dispatch({ type: "DECREMENT", amount: 10 });
+store.dispatch({ type: "CHANGE_NAME", new_name: "David" });
+store.dispatch({ type: "CHANGE_AGE", new_age: 26 });
+
+const App = () => {
+  console.log(store.getState());
+
+  return (
+    <div className="container">
+      <h2>hello redux!</h2>
+      <ul>
+        <li>username: {store.getState().user.name}</li>
+        <li>counter: {store.getState().counter.count}</li>
+      </ul>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./App";
-// import Game from "./examples/tictactoe";
-// import App from "./examples/recipe-app/App";
-// import data from "./examples/recipe-app/data";
-// import App from "./examples/table/App";
-// import carsData from "./examples/table/data";
-import App from "./examples/modal/app";
 import "./index.css";
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
