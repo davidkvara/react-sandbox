16bc47b1487040d48445e96517b25ec04ca2bbb7
davidkvara <davekvaratskhelia@gmail.com>
2018-04-01 18:06:11 +0400

    redux file structure

--- src/App.js
+++ src/App.js
@@ -1,56 +1,29 @@
 import React from "react";
-import { combineReducers, createStore } from "redux";
+import { connect } from "react-redux";
 
-const counter = (state = { count: 0 }, action) => {
-  if (action.type === "INCREMENT") {
-    return { ...state, count: state.count + action.amount };
+class App extends React.Component {
+  clickCountUp = () => {
+    this.props.dispatch({ type: "DEC_COUNT", by: 1 });
+  };
+
+  clickCountDown = () => {
+    this.props.dispatch({ type: "INC_COUNT", by: 1 });
+  };
+
+  render() {
+    // console.log(this.props);
+    return (
+      <div>
+        <h2>{this.props.count}</h2>
+        <button onClick={this.clickCountDown}>Dec Count</button>
+        <button onClick={this.clickCountUp}>Inc Count</button>
+      </div>
+    );
   }
-  if (action.type === "DECREMENT") {
-    return { ...state, count: state.count - action.amount };
-  }
-  return state;
-};
+}
 
-const initialState = {
-  name: "user",
-  age: null
-};
+function mapStateToProps(state) {
+  return { count: state.count };
+}
 
-const user = (state = initialState, action) => {
-  if (action.type === "CHANGE_NAME") {
-    return { ...state, name: action.new_name };
-  }
-  if (action.type === "CHANGE_AGE") {
-    return { ...state, age: action.new_age };
-  }
-  return state;
-};
-
-const reducer = combineReducers({
-  counter: counter,
-  user: user
-});
-
-const store = createStore(reducer);
-
-store.dispatch({ type: "INCREMENT", amount: 5 });
-store.dispatch({ type: "INCREMENT", amount: 15 });
-store.dispatch({ type: "DECREMENT", amount: 10 });
-store.dispatch({ type: "CHANGE_NAME", new_name: "David" });
-store.dispatch({ type: "CHANGE_AGE", new_age: 26 });
-
-const App = () => {
-  console.log(store.getState());
-
-  return (
-    <div className="container">
-      <h2>hello redux!</h2>
-      <ul>
-        <li>username: {store.getState().user.name}</li>
-        <li>counter: {store.getState().counter.count}</li>
-      </ul>
-    </div>
-  );
-};
-
-export default App;
+export default connect(mapStateToProps)(App);
