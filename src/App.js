91d0a33688d6cf663291f6781312973b0dce6ea1
davidkvara <davekvaratskhelia@gmail.com>
2018-05-15 19:34:50 +0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React from "react";
 
-const App = props => {
-  return (
-    <div style={props.layout}>
-      <h1>Hello, Again</h1>
-      <p>
-        Accusamus facere libero, molestiae rem odit inventore est temporibus
-        aliquid sunt quod voluptatem voluptas dignissimos, beatae possimus
-        reprehenderit amet aspernatur molestias at assumenda expedita hic.
-        Error, nemo hic voluptatem sunt totam amet eos!
-      </p>
-    </div>
-  );
-};
-
-export default App;
+export default () => (
+  <React.Fragment>
+    <h2 style={{ padding: 30, margin: 0 }}>React Sandbox</h2>
+  </React.Fragment>
+);
