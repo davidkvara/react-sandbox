348341d263ec43a5f9be8058969f40b48787580d
davidkvara <davekvaratskhelia@gmail.com>
2018-06-27 23:52:27 +0400

    refactor, lift state up to hide scroll down sign

--- src/learn/ScrollEvent/index.js
+++ src/learn/ScrollEvent/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import "./style.css";
 
-class Header extends Component {
+export default class App extends React.Component {
   state = { elasticHeader: false };
 
   componentDidMount() {
@@ -13,65 +13,61 @@ class Header extends Component {
   }
 
   handleScroll = () => {
-    // cool cool cool
     let scrollTop = window.scrollY;
+    // ეს არის ყველაზე "efficient"
 
-    if (scrollTop === 0 && this.state.elasticHeader === true) {
+    if (scrollTop === 0 && this.state.elasticHeader) {
       this.setState({ elasticHeader: false });
-    } else if (scrollTop !== 0 && this.state.elasticHeader !== true) {
+    } else if (scrollTop !== 0 && !this.state.elasticHeader) {
       this.setState({ elasticHeader: true });
     }
   };
-
   render() {
     const { elasticHeader } = this.state;
-    // console.log("rendered, elasticHeader:", elasticHeader);
-
     return (
-      <header className={elasticHeader ? "header scrolled" : "header"}>
-        Header {elasticHeader ? "➖" : "➕"}
-      </header>
+      <div className="app">
+        <Header elastic={elasticHeader} />
+        <main className="main">
+          <div className="container">
+            <h2>Elastic header</h2>
+            <p>
+              Dicta dolorum est itaque aliquam molestias commodi aliquid minima
+              sint non. Quos sed tempore aliquid expedita quis. Nostrum,
+              asperiores sunt voluptatem eos distinctio similique impedit sequi
+              ipsam magni, non reprehenderit.
+            </p>
+            <p>
+              Corporis itaque vitae quae dolorum dolore eius ipsam corrupti
+              dolor ducimus, labore cumque eligendi iste voluptates
+              exercitationem sed iusto officia eveniet eaque provident quisquam
+              unde fugiat? Nulla aperiam expedita eligendi.
+            </p>
+            <p>
+              Nisi quasi quod eius vero. Impedit necessitatibus delectus
+              reiciendis consequatur voluptatem provident, enim optio aut ullam.
+              Labore, cumque adipisci explicabo dolorum, aut expedita minima
+              quam maxime unde harum delectus ratione?
+            </p>
+          </div>
+        </main>
+        {elasticHeader || <div className="sign">Scroll Down</div>}
+        <Footer />
+      </div>
     );
   }
 }
 
-const App = () => {
+const Header = props => {
+  // console.log("rendered, elastic:", elastic);
   return (
-    <div className="app">
-      <Header />
-      <main className="main">
-        <div className="container">
-          <h2>Elastic header</h2>
-          <p>
-            Dicta dolorum est itaque aliquam molestias commodi aliquid minima
-            sint non. Quos sed tempore aliquid expedita quis. Nostrum,
-            asperiores sunt voluptatem eos distinctio similique impedit sequi
-            ipsam magni, non reprehenderit.
-          </p>
-          <p>
-            Corporis itaque vitae quae dolorum dolore eius ipsam corrupti dolor
-            ducimus, labore cumque eligendi iste voluptates exercitationem sed
-            iusto officia eveniet eaque provident quisquam unde fugiat? Nulla
-            aperiam expedita eligendi.
-          </p>
-          <p>
-            Nisi quasi quod eius vero. Impedit necessitatibus delectus
-            reiciendis consequatur voluptatem provident, enim optio aut ullam.
-            Labore, cumque adipisci explicabo dolorum, aut expedita minima quam
-            maxime unde harum delectus ratione?
-          </p>
-        </div>
-      </main>
-      <div className="sign">Scroll Down</div>
-      <Footer />
-    </div>
+    <header className={props.elastic ? "header scrolled" : "header"}>
+      Header {props.elastic ? "➖" : "➕"}
+    </header>
   );
 };
 
 const Footer = () => {
-  console.log("rendered footer");
-
+  // არ ღირს pureComponent-ის გამოყენება
+  // console.log("rendered footer");
   return <footer className="footer">@TheEnd</footer>;
 };
-
-export default App;
