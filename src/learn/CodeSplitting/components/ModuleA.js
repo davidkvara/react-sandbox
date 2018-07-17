e5f7d59551fae43a8993f66cb3d860fa97c5d147
davidkvara <davekvaratskhelia@gmail.com>
2018-07-17 22:42:11 +0400

    better with inline styles

--- src/learn/CodeSplitting/components/ModuleA.js
+++ src/learn/CodeSplitting/components/ModuleA.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 
 export default class Widget extends React.Component {
   state = { number: 300 };
@@ -29,11 +28,15 @@ export default class Widget extends React.Component {
             fontSize: 22
           }}
         >
-          <Button onClick={this.increment}>+</Button>
+          <button style={button} onClick={this.increment}>
+            +
+          </button>
           <div style={{ minWidth: 100, textAlign: "center", fontWeight: 600 }}>
             <mark>{this.state.number}</mark>
           </div>
-          <Button onClick={this.decrement}>-</Button>
+          <button style={button} onClick={this.decrement}>
+            -
+          </button>
         </div>
         <p>
           The best way to introduce code-splitting into your app is through the
@@ -44,14 +47,11 @@ export default class Widget extends React.Component {
   }
 }
 
-const Button = styled.button`
-  font-size: 24px;
-  padding: 10px 20px;
-  background: #fff;
-  width: 60px;
-  border: 1px solid #ddd;
-  border-radius: 3px;
-  &:focus {
-    background: #f9f9f9;
-  }
-`;
+const button = {
+  fontSize: "1.5rem",
+  padding: "10px 20px",
+  background: "#fff",
+  width: 60,
+  border: "1px solid #ddd",
+  borderRadius: 3
+};
