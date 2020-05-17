7b0ad6a5994cdb6e52ab77c4c72a385f0e6ef347
davidkvara <davekvaratskhelia@gmail.com>
2020-05-17 15:57:01 +0400

    react memo example

--- src/learn/Todos2/components.js
+++ src/learn/Todos2/components.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 
 export const TodosInput = ({ onSubmit, input, onInputChange }) => {
   return (
@@ -13,35 +13,43 @@ export const TodosInput = ({ onSubmit, input, onInputChange }) => {
   );
 };
 
-export const TodosList = ({ todos, onCheckboxChange, onRemoveTodo }) => {
-  return (
-    <ul>
-      {todos.map(todo => {
-        return (
-          <li
-            key={todo.id}
-            style={{
-              textDecoration: todo.completed ? "line-through" : "none"
-            }}
-          >
-            <label>
-              <input
-                type="checkbox"
-                checked={todo.completed}
-                onChange={() => onCheckboxChange(todo.id)}
-                style={{ marginRight: 10 }}
-              />
-              {todo.text}
-            </label>
-            <button
-              onClick={() => onRemoveTodo(todo.id)}
-              style={{ marginLeft: 10 }}
+export const TodosList = memo(
+  ({ todos, onCheckboxChange, onRemoveTodo }) => {
+    console.log("renders todo-list");
+
+    return (
+      <ul>
+        {todos.map(todo => {
+          return (
+            <li
+              key={todo.id}
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none"
+              }}
             >
-              &times;
-            </button>
-          </li>
-        );
-      })}
-    </ul>
-  );
-};
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => onCheckboxChange(todo.id)}
+                  style={{ marginRight: 10 }}
+                />
+                {todo.text}
+              </label>
+              <button
+                onClick={() => onRemoveTodo(todo.id)}
+                style={{ marginLeft: 10 }}
+              >
+                &times;
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  },
+  (prevProps, nextProps) => {
+    // თუ todos არ შეცვლილა არ დაარენდერო
+    return prevProps.todos === nextProps.todos;
+  }
+);
